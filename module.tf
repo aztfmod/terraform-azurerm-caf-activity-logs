733b24bd740ca7eae7df569f64f9864e7bb5bc98
Arnaud Lheureux <arnaudlh@hotmail.com>
2019-12-16 09:25:55 +0800

    Initial commit for 1912 refresh

--- module.tf
+++ module.tf
@@ -1,38 +1,46 @@
-#Defines the subscription-wide logging and eventing settings
-#Creating the containers on Storage Account and Event Hub (optional)
+# Defines the subscription-wide logging and eventing settings
+# Creating the containers on Storage Account and Event Hub (optional)
 
-resource "random_string" "name" {
-    length  = 24 - length(var.prefix)
-    upper   = false
-    special = false
+module "caf_name_st" {
+  source = "../terraform-azurerm-caf-naming/"
+  
+  name    = var.name
+  type    = "st"
+  convention  = var.convention
 }
 
-locals {
-    name = "${var.prefix}${random_string.name.result}"
+module "caf_name_evh" {
+  source = "../terraform-azurerm-caf-naming/"
+  
+  name    = var.name
+  type    = "evh"
+  convention  = var.convention
 }
 
 resource "azurerm_storage_account" "log" {
-  name                     = local.name
-  resource_group_name      = var.resource_group_name
-  location                 = var.location
-  account_kind             = "StorageV2"
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
-  access_tier              = "Hot"
+  name                      = module.caf_name_st.st
+  resource_group_name       = var.resource_group_name
+  location                  = var.location
+  account_kind              = "StorageV2"
+  account_tier              = "Standard"
+  account_replication_type  = "GRS"
+  access_tier               = "Hot"
   enable_https_traffic_only = true
-  tags                     = local.tags
+  tags                      = local.tags
 }
 
 resource "azurerm_eventhub_namespace" "log" {
   count = var.enable_event_hub ? 1 : 0 
-  name                = local.name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  sku                 = "Standard"
-  capacity            = 2
-  tags                = local.tags
-  auto_inflate_enabled = false
-  kafka_enabled       = true
+  
+  name                    = module.caf_name_evh.evh
+  location                = var.location
+  resource_group_name     = var.resource_group_name
+  sku                     = "Standard"
+  capacity                = 2
+  tags                    = local.tags
+  auto_inflate_enabled    = false
+  # kafka_enabled         = true
+
 }
 
 resource "azurerm_monitor_log_profile" "subscription" {
@@ -46,7 +54,7 @@ resource "azurerm_monitor_log_profile" "subscription" {
 
 # Add all regions - > put in variable
 # az account list-locations --query '[].name' 
-# updated Nov 08 2019 
+# updated Dec 15 2019 
   locations = [
   "global",
   "eastasia",
